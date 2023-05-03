a490d94e7f3697617e76ef45f283f322c8fec6b3
developerhimanshu <chamyalhemsingh@gmail.com>
2023-05-03 19:08:19 +0530

    Done some changes

--- src/components/problems.tsx
+++ src/components/problems.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 let problems = [
   {
@@ -12,8 +12,21 @@ let problems = [
     difficulty: "Easy",
   },
 ];
+let problems2 = [
+  {
+    name: "p1",
+    acceptance: "60%",
+    difficulty: "Hard",
+  },
+  {
+    name: "p2",
+    acceptance: "80%",
+    difficulty: "Easy",
+  },
+];
 function Problems() {
-  let problemhtml = problems.map((problem) => {
+  const [currProblems, setCurrProblems] = useState(problems);
+  let problemhtml = currProblems.map((problem) => {
     return (
       <tr>
         <td>{problem.name}</td>
@@ -25,6 +38,8 @@ function Problems() {
   return (
     <div style={{ width: "100vw" }}>
       <table width="80%">{problemhtml}</table>
+      <button onClick={() => setCurrProblems(problems)}>1</button>
+      <button onClick={() => setCurrProblems(problems2)}>2</button>
     </div>
   );
 }
